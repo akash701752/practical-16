887ca86e99e784ba0677ae4b2e01ada974d3bd70
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2022-01-04 12:39:17 +0530

    Create t2 part3 P16.cpp

---
+++ t2 part3 P16.cpp
@@ -0,0 +1,24 @@
+/*  
+    Practical 16
+    Que 16.2  : 
+    Task c    : Predict the Output
+*/ 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"\t\t Akash Yadav 200111076 A "<<"\n" ;
+
+    try {
+    throw 'a';
+    }
+    catch (int x) {
+    cout << "Caught ";
+    }
+    return 0;
+}
+/*
+    
+*/
